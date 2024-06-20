6b78f9bd09595d6c42d06c63b62bdd91e794985f
saravatss <rebback89@gmail.com>
2024-06-20 16:30:27 +0300

    post

--- src/routers/books.js
+++ src/routers/books.js
@@ -1,4 +1,3 @@
-import { readFileSync } from 'node:fs';
 import { Router, DBJson } from '../../core/index.js';
 
 const booksRouter = new Router();
@@ -8,13 +7,66 @@ booksRouter.get('/books', (request, response) => {
         'Content-type': 'application/json'
     });
 
-    const DBBooks = new DBJson('./src/db/books.json');
+    const DBBooks = new DBJson('./src/db/books.json'); 
 
     response.end(JSON.stringify(DBBooks.getAll()));
+});
+
+booksRouter.post('/books', (request, response) => {
+    response.writeHead(200, {
+        'Content-type': 'application/json'
+    });
+
+    let data = '';
+
+    request
+        .on('data', (chunck) => {
+            data += chunck;
+        })
+        .on('end', () => {
+            const newBook = JSON.parse(data);
+
+            const DBBooks = new DBJson('./src/db/books.json');
+
+            const success = DBBooks.add(newBook);
+
+            if (success) {
+                response.end(JSON.stringify(DBBooks.getAll()));
+            } else {
+                response.end('Не удалось добавить книгу');
+            }
+        });
+});
+
+booksRouter.put('/books', (request, response) => {
+    response.writeHead(200, {
+        'Content-type': 'application/json'
+    });
+
+    let data = '';
+
+    request
+        .on('data', (chunck) => {
+            data += chunck;
+        })
+        .on('end', () => {
+            const url = new URL('http://127.0.0.1:8080' + request.url);
+            const queryId = parseInt(url.searchParams.get('id'));
+
+            const payload = JSON.parse(data);
+
+            const DBBooks = new DBJson('./src/db/books.json');
+
+            const success = DBBooks.updateById(queryId, payload);
+
+            if (success) {
+                response.end(JSON.stringify(DBBooks.getAll()));
+            } else {
+                response.end('Не удалось обновить данные книги');
+            }
+        });
 });
 
 export {
     booksRouter
 };
-
-// написать post функцию !!! 
